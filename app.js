59bcd474f6a03da590233ced79d13d0f42a516a2
William182-17s <78028635+William182-17s@users.noreply.github.com>
2023-05-04 11:43:03 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+function getCookie(cname) {
+  		let name = cname + "=";
+  		let decodedCookie = decodeURIComponent(document.cookie);
+  		let ca = decodedCookie.split(';');
+  		for(let i = 0; i <ca.length; i++) {
+    			let c = ca[i];
+    			while (c.charAt(0) == ' ') {
+      				c = c.substring(1);
+    			}
+    			if (c.indexOf(name) == 0) {
+      				return c.substring(name.length, c.length);
+    			}
+  		}
+  		return "";
+	}
+	
+	let clientId = getCookien('clientId')
+	let accessToken = getCookie('accToken');
+	let instanceUrl = getCookie('instanceUrl')
+	console.log("accessToken: " + accessToken);
+	console.log("instanceUrl: " + instanceUrl);
+	
+	
+	let oauth = OAuth.createInstance(clientId
+                                ,"https://test.salesforce.com",
+                                "https://infinite-brushlands-64356.herokuapp.com/oauthcallback.html"
+                                );
+	oauth.login()
+    	.then(oauthResult => {
+        	console.log(oauthResult);
+    	});
+	
+$Lightning.use("c:AppointmentTakingApp", function() {
+        $Lightning.createComponent("c:appointmentHandler",
+            { recordId : 'a1Z1x00000413RKEAY' },
+            "AppointmentHandler",
+            function() {
+                console.log('LWC Componenet added in VF page'); 
+            }
+        );
+    }, 
+	instanceUrl,
+	 accessToken
+);
